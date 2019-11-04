8a37f217870b451056fa71a6058c1e6542de8e4e
t-90ribeiro-byte <56850912+t-90ribeiro-byte@users.noreply.github.com>
2019-11-04 20:27:26 -0300

    Update Fatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -1,5 +1,5 @@
 public class Fatorial{
-
+	
 public static void main (String [] args){
 	
 	int i, Fat = 10;
@@ -11,29 +11,4 @@ public static void main (String [] args){
 	     	}System.out.print(Fat);
 	     	
 		}
-	}
-    
-    import java.util.Scanner;
-
-public class Exercicio03 {
-
-    public static void main(String[] args){
-    
-        Scanner ent = new Scanner(System.in);
-        int num, fat = 1;
-        int cont = 1;
-
-        do{
-            System.out.println("Digite um nº");
-            num = ent.nextInt();
-            
-            for(int i = 1;i <= num; i++){
-                fat = fat * i;
-            }
-            
-            System.out.println("!" + num + " = " + fat);
-            cont++;
-            
-       }while(cont < 2);
-    }
 }
